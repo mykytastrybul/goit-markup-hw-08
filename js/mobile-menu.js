76c10f062eba7dab3e4c40454a7e737c14ba4b0f
s3bull <95282477+s3bull@users.noreply.github.com>
2022-01-27 16:07:41 +0200

    Changes

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,17 +1,12 @@
+(() => {
+    const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobileMenuRef = document.querySelector("[data-menu]");
+    menuBtnRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-const menuBtnRef = document.querySelectorAll('[data-menu-button]');
-const mobileMenuRef = document.querySelector('[data-menu]');
-
-menuBtnRef.forEach(btn => {
-btn.addEventListener('click', () => {
-toggleMobMenu(btn);
-});
-});
-
-function toggleMobMenu(btn) {
-const expanded = btn.getAttribute('aria-expanded') === 'true' || false;
-btn.classList.toggle('is-open');
-btn.setAttribute('aria-expanded', !expanded);
-
-mobileMenuRef.classList.toggle('is-open');
-} 
+        mobileMenuRef.classList.toggle("is-open");
+    })
+})();
